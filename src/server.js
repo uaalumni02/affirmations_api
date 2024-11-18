4fc046d662791be3a1c9342bb8c80166264fb9a5
uaalumni02 <dbell1911@gmail.com>
2024-11-18 17:52:16 -0600

    connect db

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose, { connect } from "mongoose";
 
 const app = express();
 const { log, error } = console;
@@ -7,6 +10,20 @@ const { log, error } = console;
 const port = process.env.PORT || 3000;
 
 const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const DB_URL = process.env.MONGO_URL;
+
+try {
+  mongoose.connect(DB_URL, { useNewUrlParser: true });
+  console.log("Connection Successful");
+} catch (err) {
+  console.error("Unable to Connect to MongoDB", err);
+}
 
 
 app.use("/api", router);
